ff8939e87997f83463a6b729e680d952ad6ab400
cslane64 <cslane@gmail.com>
2019-06-15 10:52:33 -0400

    changed event table to store event zip

--- models/event.js
+++ models/event.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var Event = sequelize.define("event", {
     eventName: DataTypes.STRING,
     eventDate: DataTypes.DATE,
-    eventTime: DataTypes.STRING,
+    eventZip: DataTypes.STRING,
     eventDescription: DataTypes.TEXT
   });
   return Event;
